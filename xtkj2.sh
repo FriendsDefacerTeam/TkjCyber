67d2ef56e421bc1025c06704acdeb0787c703556
Mr. Mpx7 Aril Al Farel <56549838+FriendsDefacerTeam@users.noreply.github.com>
2019-10-20 10:07:36 +0700

    Add files via upload

---
+++ xtkj2.sh
@@ -0,0 +1,833 @@
+#!/bin/sh
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+clear
+sleep 1
+figlet -f standard "LOGIN DULU" | lolcat
+date | lolcat
+echo
+echo -n $red"[" $blue"ENTER YOUR PASSWORD "$red"]" $cyan": "
+read pwd;
+
+if [ $pwd = "abdi" ]
+then
+    sleep 1
+    clear
+    echo $green "        WELLCOME TO MY TOOL5 MR."
+else
+    sleep 1
+    echo $yellow "MASUKKAN PASSWORD YANG BENAR NJENK!1!1!"
+    echo $white "LOGIN KEMBALI"
+    sleep 1
+    sh xtkj2.sh
+
+fi
+echo $cyan "||||||||||||||||||||||||||||||||||||||||||"
+echo $green "____________________________________________"
+echo $yellow "Author  :"$blue"Mr. Mpx7"
+echo $yellow "Team    :"$blue"FR13NDS DEFACER TEAM"
+echo $yellow "contack :"$blue"abdikomputer936@gmail.com"
+echo $yellow "Tools   :"$blue"xtkj2cyberteam"
+echo $green "____________________________________________"
+echo $yellow "_________________________________________"
+echo $cyan "|||||||||||||||"$red"["$white" MENU VIP "$red"]"$cyan"|||||||||||||||"
+echo $yellow "_________________________________________"
+echo
+echo $red"        [" $blue "JANGAN DI RECODE YA NJENK "  $red"]"
+echo
+echo $cyan"["$yellow "A"$cyan "]" $white"ABOUT INFORMATION AUTHOR"
+echo $cyan"["$yellow "B"$cyan "]" $white"ABOUT THIS TOOLS"
+echo $white"[01] Install OWASP-ZSC"
+echo "[02] Install InfoGa"
+echo "[03] Install cok-rat"
+echo "[04] Install hash-Generator"
+echo "[05] Install Open Source Information Facebook"
+echo "[06] Install str2bf"
+echo "[07] Install BForce"
+echo "[08] Install Web Application Audit Framework"
+echo "[09] Install SMBrute"
+echo "[10] Install Wordpress Security Scanner"
+echo "[11] Install hmap (fingerprinting web servers)"
+echo "[12] Install Auto Report Facebook"
+echo "[13] Install dhcpdorf"
+echo "[14] Install deskcon-desktop"
+echo "[15] Install Tools suRAT-Cinta"
+echo "[16] Install Tools m4ngARP"
+echo "[17] Install Tools WP-Hunter"
+echo "[18] Install Remote-Shell"
+echo "[19] Install LITESCRIPT "
+echo "[20] Install meTAInstall"
+echo "[21] Install Zilcorili (Security Framework) "
+echo "[22] Install AutoScriptKiddiesTool "
+echo "[23] Install bot-exploiters"
+echo "[24] Install Termux-metasploit [work] "
+echo "[25] Install Instagram Bot"
+echo "[26] Install FesbukBrute"
+echo "[27] Install Email-Harvester"
+echo "[28] Install Gdog Backdoor Using Gmail"
+echo "[29] Install Penetration Testing Engagements"
+echo "[30] Install CanisRufus"
+echo "[31] Install EttercapForAndroid"
+echo "[32] Install  BlackNmap"
+echo "[33] Install NMapForAndroid"
+echo "[34] Install 1337Rat "
+echo "[35] Install Universal Fedora Chroot Tool"
+echo "[36] Install Knock Subdomain Scan  "
+echo "[37] Install  OWASP-vbscan"
+echo "[38] Install Dracnmap"
+echo "[39] Install Tool Brutal "
+echo "[40] Install BruteSploit "
+echo "[41] Install Microsploit"
+echo "[42] Install Vegile"
+echo "[43] Install PhEmail"
+echo "[44] Install reposcanner"
+echo "[45] Install search drives for credit card numbers "
+echo "[46] Install zambie"
+echo "[47] Install TrackUrl"
+echo "[48] Install DataSploit "
+echo "[49] Install E-Bomber"
+echo "[50] Install IP-Locator"
+echo $red"[00] EXITING PROGRAM"
+echo $green""
+read -p "xTkj2>>" bro
+
+if [ $bro = A ] || [ $bro = a ]
+then
+clear
+sleep 1
+echo $white"[]+++++++++++++++++++++++++++++++++++++++++[]"
+echo $blue"        ["     $cyan"ABOUT INFORMATION AUTHOR"       $blue"]"
+echo $white"[]+++++++++++++++++++++++++++++++++++++++++[]"
+echo
+echo $white"========================================"
+sleep 2
+echo $cyan"Nama    :"$white"Muhammad Abdillah"
+sleep 1
+echo $cyan"Kelas   :"$white"X T.K.J 2"
+sleep 1
+echo $cyan"Sekolah :"$white"SMK N 1 DOLOK MASIHUL"
+sleep 1
+echo $cyan"Wali Kelas :"$white"Buk Anita"
+sleep 1
+echo $cyan"Ketua Kelas :"$white"Muhammad Abdillah"
+sleep 1
+echo $cyan"Wakil Ketua Kelas :"$white"Hary Amsyah"
+sleep 1
+echo $cyan"Sekertaris kelas :"$white"Nanda Riyani"
+sleep 1
+echo $cyan"Bandahara Kelas :"$white"Lyra Sri Mandani"
+sleep 1
+echo $cyan"Angggota :"$white"Seluruh Warga Kelas X T.K.J 2"
+sleep 1
+echo $cyan"Nama-Nama Anggota :  "
+sleep 1
+echo
+echo $white"1. angga saputra"
+echo "2. ayu lestari"
+echo "3. ade arista"
+echo "4. halimahtusaqdiah"
+echo "5. Ridho carlei naibaho"
+echo "6. weril nainggolan"
+echo "7. Dio armanda"
+echo "8. Melisa Hardining"
+echo "9. melisa utabarat"
+echo "10. muhammad diki anugrah"
+echo "11. muhammad ziyan akbar"
+echo "12. muhammad asri koto"
+echo "13. Rinaldi"
+echo "14. parulina butar-butar"
+echo "15. Dera rafika"
+echo "16. nasta alessandro tambun"
+echo "17. pria dimas"
+echo "18. ToMI"
+echo "19. Riska sabila"
+echo "20. Ria anggraini"
+echo "21. Eva"
+echo "22. Johannes sijabat"
+echo "23. Endah puji asnita"
+echo "24. nur rahma dinda pane"
+echo "25. Nia Septiana rahma"
+echo "26. Lutviah andini"
+echo "28. winda purba"
+echo "29. joya"
+echo "30. sinta"
+echo "31. siska sirmamata"
+echo "32. Ignatia"
+echo $white"========================================"
+fi
+
+if [ $bro = B ] || [ $bro = b ]
+then
+clear
+echo $yellow"Assalamu'Alaikum"
+sleep 1
+echo $white"Tools Ini Saya Buat Bertujuan Untuk"
+sleep 1
+echo $white"Membuat Sekolah SMK N 1 Dolok Masihul Sekaligus "
+sleep 1
+echo $white"jurusan TEKNIK KOMPUTER DAN JARINGAN"
+sleep 1
+echo $white"Di Kenal Di Masyarakat Dan Juga Membawa"
+sleep 1
+echo $white"Nama Kelas X T.K.J 2 Menjadi Lebih"
+sleep 1
+echo $white"Baik Lagi"
+sleep 1
+echo $white"Thanks,,,,,"
+fi
+
+if [ $bro = 01 ] || [ $bro = 1 ]
+then
+clear
+apt update 
+apt upgrade
+apt install git 
+apt install python2
+git clone https://github.com/zdresearch/OWASP-ZSC
+mv OWASP-ZSC $HOME
+cd $HOME/OWASP-ZSC
+python2 installer.py 
+fi
+
+if [ $bro = 02 ] || [ $bro = 2 ]
+then
+clear
+apt update 
+apt upgrade
+apt install git 
+apt install php
+git clone https://github.com/mrcakil/InfoGa
+mv InfoGa $HOME
+cd $HOME/InfoGa
+php infoga.php
+fi
+
+if [ $bro = 03 ] || [ $bro = 3 ]
+then
+clear
+apt upgrade
+apt install git 
+git clone https://github.com/mrcakil/cok-Rat
+mv cok-Rat $HOME
+cd $HOME/cok-Rat
+unzip rat.zip
+ls
+fi
+
+if [ $bro = 04 ] || [ $bro = 4 ]
+then
+clear
+apt update 
+apt upgrade
+apt install git 
+apt install python2
+git clone https://github.com/CiKu370/hash-generator
+mv hash-generator $HOME
+cd $HOME/hash-generator
+python2 hashgen.py
+fi
+
+if [ $bro = 05 ] || [ $bro = 5 ]
+then
+apt upgrade
+apt install git 
+apt install python2
+git clone https://github.com/CiKu370/OSIF
+mv OSIF $HOME
+cd $HOME/OSIF
+python2 osif.py
+fi
+
+if [ $bro = 06 ] || [ $bro = 6 ]
+then
+apt update 
+apt upgrade
+apt install git 
+apt install python2
+git clone https://github.com/CiKu370/str2bf
+mv str2bf $HOME
+cd $HOME/str2bf
+python2 str2bf.py
+fi
+
+if [ $bro = 07 ] || [ $bro = 7 ]
+then
+clear
+apt update 
+apt upgrade
+apt install git 
+apt install python2
+git clone https://github.com/YukersCreew/BForce
+mv BForce $HOME
+cd $HOME/BForce
+python2 Yukers.py
+fi
+
+if [ $bro = 08 ] || [ $bro = 8 ]
+then
+clear
+apt update 
+apt upgrade
+apt install git 
+apt install python2
+git clone https://github.com/m4ll0k/Galileo
+mv Galileo $HOME
+cd $HOME/Galileo
+pip2 install -r requirements.txt
+python2 galileo.py
+fi
+
+if [ $bro = 09 ] || [ $bro = 9 ]
+then
+clear
+apt update 
+apt upgrade
+apt install git 
+apt install python2
+git clone https://github.com/m4ll0k/SMBrute
+mv SMBrute $HOME
+cd $HOME/SMBrute
+python2 smbrute.py
+fi
+
+if [ $bro = 10 ] || [ $bro = 10 ]
+then
+clear
+apt update 
+apt upgrade
+apt install git 
+apt install python2
+git clone https://github.com/m4ll0k/WPSeku
+mv WPSeku $HOME
+cd $HOME/WPSeku
+pip2 install -r requirements.txt
+python2 wpseku.py
+fi
+
+if [ $bro = 11 ] || [ $bro = 11 ]
+then
+clear
+apt update 
+apt upgrade
+apt install git 
+apt install python2
+git clone https://github.com/Mebus/hmap
+mv hmap $HOME
+cd $HOME/hmap
+python2 hmap.py -h
+fi
+
+if [ $bro = 12 ] || [ $bro = 12 ]
+then
+clear
+apt update 
+apt upgrade
+apt install git 
+apt install ruby
+git clone https://github.com/gshofficialgithubindonesia/autoreport-fb
+mv autoreport-fb $HOME
+cd $HOME/autoreport-fb
+ruby autoreport-fb.rb
+fi
+
+if [ $bro = 13 ] || [ $bro = 13 ]
+then
+clear
+apt update 
+apt upgrade
+apt install git 
+apt install golang
+git clone https://github.com/Mebus/dhcpdorf
+mv dhcpdorf $HOME
+cd $HOME/dhcpdorf
+golang dhcpdorf.go
+fi
+
+if [ $bro = 14 ] || [ $bro = 14 ]
+then
+clear
+apt update 
+apt upgrade
+apt install git 
+apt install python2
+git clone https://github.com/Mebus/deskcon-desktop
+mv deskcon-desktop $HOME
+cd $HOME/deskcon-desktop
+sh deskcon.sh
+fi
+
+if [ $bro = 15 ] || [ $bro = 15 ]
+then
+clear
+apt update 
+apt upgrade
+apt install git 
+git clone https://github.com/aryanrtm/suRAT-cinta
+mv suRAT-cinta $HOME
+cd $HOME/suRAT-cinta
+sh suRAT.sh
+fi
+
+if [ $bro = 16 ] || [ $bro = 16 ]
+then
+clear
+apt update 
+apt upgrade
+apt install git 
+git clone https://github.com/aryanrtm/m4ngARP
+mv m4ngARP $HOME
+cd $HOME/m4ngARP
+sh m4ngARP.sh
+fi
+
+if [ $bro = 117 ] || [ $bro = 117 ]
+then
+clear
+apt update 
+apt upgrade
+apt install git 
+apt install perl
+git clone https://github.com/aryanrtm/WP-Hunter
+mv WP-Hunter $HOME
+cd $HOME/WP-Hunter
+perl wphunter.pl
+fi
+
+if [ $bro = 18 ] || [ $bro = 18 ]
+then
+clear
+apt update 
+apt upgrade
+apt install git
+apt install perl
+git clone https://github.com/aryanrtm/Remote-Shell
+mv Remote-Shell $HOME
+cd $HOME/Remote-Shell
+perl remoteshell.pl
+fi
+
+if [ $bro = 19 ] || [ $bro = 19 ]
+then
+clear
+apt update 
+apt upgrade
+apt install git
+apt install perl
+git clone https://github.com/4L13199/LITESCRIPT
+mv LITESCRIPT $HOME
+cd $HOME/LITESCRIPT
+python2 LITESCRIPT.py
+fi
+
+if [ $bro = 20 ] || [ $bro = 20 ]
+then
+clear
+apt update 
+apt upgrade
+apt install git
+apt install perl
+git clone https://github.com/4L13199/meTAInstall
+mv meTAInstall $HOME
+cd $HOME/meTAInstall
+chmod +x meTAInstall
+chmod 777 meTAInstall
+./meTAInstall
+fi
+
+if [ $bro = 21 ] || [ $bro = 21 ]
+then
+clear
+apt upgrade
+apt install git
+apt install ruby
+git clone https://github.com/b3-v3r/Zilcorili
+mv Zilcorili $HOME
+cd $HOME/Zilcorili
+ruby main.rb
+fi
+
+if [ $bro = 22 ] || [ $bro = 22 ]
+then
+clear
+apt update 
+apt upgrade
+apt install git
+apt install ruby
+git clone https://github.com/b3-v3r/ASKT-AutoScriptKiddiesTool-
+mv ASKT-AutoScriptKiddiesTool- $HOME
+cd $HOME/ASKT-AutoScriptKiddiesTool-
+python2 askt.py
+fi
+
+if [ $bro = 23 ] || [ $bro = 23 ]
+then
+clear
+apt update 
+apt upgrade
+apt install git
+git clone https://github.com/verluchie/bot-exploiter
+mv bot-exploiter $HOME
+cd $HOME/bot-exploiter
+unzip xploit.zip
+fi
+
+if [ $bro = 24 ] || [ $bro = 24 ]
+then
+clear
+apt upgrade
+apt install git
+git clone https://github.com/verluchie/termux-metasploit
+mv termux-metasploit $HOME
+cd $HOME/termux-metasploit
+sh install.sh
+fi
+
+if [ $bro = 25 ] || [ $bro = 25 ]
+then
+clear
+apt update 
+apt upgrade
+apt install git
+apt install python2
+git clone https://github.com/verluchie/instabot.py
+mv  instabot.py $HOME
+cd $HOME/instabot.py
+pip2 install -r requirements.txt
+python2 example.py
+fi
+
+if [ $bro = 26 ] || [ $bro = 26 ]
+then
+clear
+apt update 
+apt upgrade
+apt install git
+apt install python2
+git clone https://github.com/verluchie/fbbrute
+mv fbbrute $HOME
+cd $HOME/fbbrute
+python2 fbbrute.py
+fi
+
+if [ $bro = 27 ] || [ $bro = 27 ]
+then
+clear
+apt update 
+apt upgrade
+apt install git
+apt install python2
+git clone https://github.com/maldevel/EmailHarvester
+mv EmailHarvester $HOME
+cd $HOME/EmailHarvester
+pip2 install -r requirements.txt
+python2 EmailHarvester.py
+fi
+
+if [ $bro = 28 ] || [ $bro = 28 ]
+then
+clear
+apt update 
+apt upgrade
+apt install git
+apt install python2
+git clone https://github.com/maldevel/gdog
+mv gdog $HOME
+cd $HOME/gdog
+pip2 install -r requirements.txt
+python2 gdog.py
+fi
+
+if [ $bro = 29 ] || [ $bro = 29 ]
+then
+clear
+apt update 
+apt upgrade
+apt install git
+apt install sudo
+git clone https://github.com/maldevel/PenTestKit
+mv PenTestKit $HOME 
+cd $HOME/PenTestKit
+sudo pip install -r requirements.txt
+ls
+fi
+
+if [ $bro = 30 ] || [ $bro = 30 ]
+then
+clear
+apt update 
+apt upgrade
+apt install git
+apt install python2
+git clone https://github.com/maldevel/canisrufus
+mv canisrufus $HOME
+cd $HOME/canisrufus
+pip2 install -r requirements.txt
+python2 canisrufus.py
+fi
+
+if [ $bro = 31 ] || [ $bro = 31 ]
+then
+clear
+apt update 
+apt upgrade
+apt install git
+git clone https://github.com/Xi4u7/EttercapForAndroid
+mv EttercapForAndroid $HOME
+cd $HOME/EttercapForAndroid
+sh install.sh
+fi
+
+if [ $bro = 32 ] || [ $bro = 32 ]
+then
+clear
+apt upgrade
+apt install git
+git clone https://github.com/Xi4u7/BlackNmap
+mv BlackNmap $HOME
+cd $HOME/BlackNmap
+chmod 777 blacknmap
+./blacknmap
+fi
+
+if [ $bro = 33 ] || [ $bro = 33 ]
+then
+clear
+apt update 
+apt upgrade
+apt install git
+git clone https://github.com/Xi4u7/NMapForAndroid
+mv NMapForAndroid $HOME
+cd $HOME/NMapForAndroid
+sh install.sh
+fi
+
+if [ $bro = 34 ] || [ $bro = 34 ]
+then
+clear
+apt update 
+apt upgrade
+apt install git
+git clone https://github.com/Xi4u7/1337Rat
+mv 1337Rat $HOME
+cd $HOME/1337Rat
+sh 1337Rat.sh
+fi
+
+if [ $bro = 35 ] || [ $bro = 35 ]
+then
+clear
+apt update 
+apt upgrade
+apt install git
+git clone https://github.com/nmilosev/anyfed
+mv anyfed $HOME
+cd $HOME/anyfed
+chmod 777 anyfed
+./anyfed
+fi
+
+if [ $bro = 36 ] || [ $bro = 36 ]
+then
+clear
+apt update 
+apt upgrade
+apt install git
+git clone https://github.com/guelfoweb/knock
+mv  knock $HOME
+cd $HOME/knock
+pip2 install -r requirements.txt
+python2 setup.py
+fi
+
+if [ $bro = 37 ] || [ $bro = 37 ]
+then
+clear
+apt update 
+apt upgrade
+apt install git
+apt install perl
+git clone https://github.com/rezasp/vbscan
+mv vbscan $HOME
+cd $HOME/vbscan
+perl vbscan.pl
+fi
+
+if [ $bro = 38 ] || [ $bro = 38 ]
+then
+clear
+apt update 
+apt upgrade
+apt install git
+git clone https://github.com/Screetsec/Dracnmap
+mv Dracnmap $HOME
+cd $HOME/Dracnmap
+sh dracnmap-v2.2.sh
+fi
+
+if [ $bro = 39 ] || [ $bro = 39 ]
+then
+clear
+apt update 
+apt upgrade
+apt install git
+git clone https://github.com/Screetsec/Brutal
+mv  Brutal $HOME
+cd $HOME/Brutal
+sh Brutal.sh
+fi
+
+if [ $bro = 40 ] || [ $bro = 40 ]
+then
+clearapt update 
+apt upgrade
+apt install git
+git clone https://github.com/Screetsec/BruteSploit
+mv BruteSploit $HOME
+cd $HOME/BruteSploit
+chmod 777 Brutesploit
+./Brutesploit
+fi
+
+if [ $bro = 41 ] || [ $bro = 41 ]
+then
+clear
+apt update 
+apt upgrade
+apt install git
+git clone https://github.com/Screetsec/Microsploit
+mv Microsploit $HOME
+cd $HOME/Microsploit
+chmod 777 Microsploit
+./Microsploit
+fi
+
+if [ $bro = 42 ] || [ $bro = 42 ]
+then
+clear
+apt update 
+apt upgrade
+apt install git
+git clone https://github.com/Screetsec/Vegile
+mv Vegile $HOME
+cd $HOME/Vegile
+chmod 777 Vegile
+./Vegile
+fi
+
+if [ $bro = 43 ] || [ $bro = 43 ]
+then
+clear
+apt update 
+apt upgrade
+apt install git
+apt install python2
+git clone https://github.com/Dionach/PhEmail
+mv PhEmail $HOME
+cd $HOME/PhEmail
+python2 phemail.py
+fi
+
+if [ $bro = 44 ] || [ $bro = 44 ]
+then
+clear
+apt update 
+apt upgrade
+apt install git
+apt install python2
+git clone https://github.com/Dionach/reposcanner
+mv reposcanner $HOME
+cd $HOME/reposcanner
+python2 reposcanner.py
+fi
+
+if [ $bro = 45 ] || [ $bro = 45 ]
+then
+clear
+apt update 
+apt upgrade
+apt install git
+apt install python2
+git clone https://github.com/Dionach/PANhunt
+mv PANhunt $HOME
+cd $HOME/PANhunt
+python2 panhunt.py
+fi
+
+if [ $bro = 46 ] || [ $bro = 46 ]
+then
+clear
+apt update 
+apt upgrade
+apt install git
+apt install python2
+git clone https://github.com/zanyarjamal/zambie
+mv zambie $HOME
+cd $HOME/zambie
+sh Installer.sh
+python2 zambie.py
+fi
+
+if [ $bro = 47 ] || [ $bro = 47 ]
+then
+clear
+apt update 
+apt upgrade
+apt install git
+git clone https://github.com/zanyarjamal/TrackUrl
+mv TrackUrl $HOME
+cd $HOME/TrackUrl
+sh TrackUrl.sh
+fi
+
+if [ $bro = 48 ] || [ $bro = 48 ]
+then
+clear
+apt upgrade
+apt install git
+apt install python2
+git clone https://github.com/zanyarjamal/DataSploit
+mv DataSploit $HOME
+cd $HOME/DataSploit
+pip2 install -r requirements.txt
+python2 datasploit.py
+fi
+
+if [ $bro = 49 ] || [ $bro = 49 ]
+then
+clear
+apt update 
+apt upgrade
+apt install git
+apt install python2
+git clone https://github.com/zanyarjamal/Email-bomber
+mv Email-bomber $HOME
+cd $HOME/Email-bomber
+chmod +x E-bomber.py
+python2 E-bomber.py
+fi
+
+if [ $bro = 50 ] || [ $bro = 50 ]
+then
+clear
+apt update 
+apt upgrade
+apt install perl
+apt install git
+git clone https://github.com/zanyarjamal/IP-Locator
+mv IP-Locator $HOME
+cd $HOME/IP-Locator
+perl ip-locator.pl
+fi
+
+if [ $bro = 00 ] || [ $bro = 0 ]
+then
+figlet -f slant Sampai | lolcat
+figlet -f slant Jumpa | lolcat
+echo $red"1"$cyan"Author : Muhammad Abdillah"
+fi
+
